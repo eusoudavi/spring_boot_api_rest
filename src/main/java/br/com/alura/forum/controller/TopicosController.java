0f1d3c0e08fc9759618f704a314563b351644ac0
davi.visintainer <davi.visintainer@placeti.com.br>
2022-11-07 14:49:48 -0300

    Ajustando a controller para responder sempre como API Rest

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -2,18 +2,16 @@ package br.com.alura.forum.controller;
 
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class TopicosController {
 
     @GetMapping("/topicos")
-    @ResponseBody // É UTILIZADA UMA BIBLIOTECA CHAMADA JACKSON PARA TRANSFORMAR A RESPOSTA EM UM JSON
     public List<Topico> listar(){
 //        TÓPICOS
         String titulo = "Dúvida";
