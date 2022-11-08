c7846645c2e69635ec42993345145076cfa7ddc3
davi.visintainer <davi.visintainer@placeti.com.br>
2022-11-08 20:32:57 -0300

    Criando método Post para enviar requisições no corpo da request

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,21 +1,26 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicosRepository topicosRepository;
 
-    @GetMapping("/topicos")
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @GetMapping
     public List<TopicoDto> listar(String nomeCurso, String titulo) {
         if (nomeCurso != null && titulo == null) {
             System.out.println(nomeCurso);
@@ -29,6 +34,12 @@ public class TopicosController {
             List<Topico> topicos = topicosRepository.findAll();
             return TopicoDto.converter(topicos);
         }
+    }
+
+    @PostMapping
+    public void cadastrar(@RequestBody TopicoForm form){
+        Topico topico = form.converter(cursoRepository);
+        topicosRepository.save(topico);
 
     }
 
