909aa4fe8eeae39997dfceb0cdd417d66276287e
davi.visintainer <davi.visintainer@placeti.com.br>
2022-11-08 20:03:59 -0300

    Pesquisas customizadas através da Repository

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,14 +1,12 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.TopicosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -18,9 +16,20 @@ public class TopicosController {
     private TopicosRepository topicosRepository;
 
     @GetMapping("/topicos")
-    public List<TopicoDto> listar(){
-        List<Topico> topicos = topicosRepository.findAll();
-        return TopicoDto.converter(topicos);
+    public List<TopicoDto> listar(String nomeCurso, String titulo) {
+        if (nomeCurso != null && titulo == null) {
+            System.out.println(nomeCurso);
+            List<Topico> topicos = topicosRepository.findByCursoNome(nomeCurso);
+            return TopicoDto.converter(topicos);
+        } else if (nomeCurso == null && titulo != null) {
+            System.out.println(titulo);
+            List<Topico> topicos = topicosRepository.findByTitulo(titulo);
+            return TopicoDto.converter(topicos);
+        } else {
+            List<Topico> topicos = topicosRepository.findAll();
+            return TopicoDto.converter(topicos);
+        }
+
     }
 
 }
--- src/main/java/br/com/alura/forum/repository/TopicosRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicosRepository.java
@@ -4,6 +4,13 @@ import br.com.alura.forum.modelo.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TopicosRepository extends JpaRepository<Topico, Long> {
+    List<Topico> findByTitulo(String titulo);
+
+    //    O JPA FAZ UMA PESQUISA NA ENTIDADE CURSO E PROCURA PELO NOME
+    //    -> PARA EVITAR AMBIGUIDADES ENTRE ATRIBUTOS E RELACIONAMENTOS, PODEMOS ESCREVER A FUNÇÃO ABAIXO COMO findByCurso_Nome
+    List<Topico> findByCursoNome(String nomeCurso);
 }
