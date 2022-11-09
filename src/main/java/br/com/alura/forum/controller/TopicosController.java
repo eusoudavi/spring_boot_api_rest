924bad381c709e4756ea6f9d5e9cd1bc130cb312
davi.visintainer <davi.visintainer@placeti.com.br>
2022-11-09 13:24:57 -0300

    ATUALIZAÇÃO

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -2,12 +2,14 @@ package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.DetalhesTopicoDto;
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.AtualTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -42,18 +44,27 @@ public class TopicosController {
     }
 
     @GetMapping("/{id}")
-    public DetalhesTopicoDto detalhar(@PathVariable Long id){
+    public DetalhesTopicoDto detalhar(@PathVariable Long id) {
         Topico topico = topicosRepository.getReferenceById(id);
         return new DetalhesTopicoDto(topico);
     }
 
     @PostMapping
-    public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder){
+    public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
         Topico topico = form.converter(cursoRepository);
         topicosRepository.save(topico);
 
         URI uri = uriBuilder.path("topicos/{id}").buildAndExpand(topico.getId()).toUri();
         return ResponseEntity.created(uri).body(new TopicoDto(topico));
+    }
+
+    //    @PatchMapping     A ANOTAÇÃO DO TIPO PATCH NORMALMENTE SERVE PARA PEQUENAS ATUALIZAÇÕES
+    @PutMapping("/{id}")     // ENQUANTO QUE A ATUALIZAÇÃO DO TIPO PUT NORMALMENTE SERVE PARA SOBRESCREVER TODO O TÓPICO
+    @Transactional              // SEM ESSA ANOTAÇÃO, O SPRING NÃO COMMITA AS ATUALIZAÇÕES NO BANCO DE DADOS
+    public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualTopicoForm form) {
+        Topico topico = form.atualizar(id, topicosRepository);
+//        QUANDO O MÉTODO ACIMA TERMINAR DE EXECUTAR, O SPRING DATA JÁ VAI ATUALIZAR O BANCO DE DADOS SEM A NECESSIDADE DE MANDAR O SISTEMA FAZER O UPDATE
+        return ResponseEntity.ok(new TopicoDto(topico));
     }
 
 }
