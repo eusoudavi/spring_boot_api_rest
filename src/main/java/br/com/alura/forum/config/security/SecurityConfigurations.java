ea2e66688d7328175a1d9200219df6c6b1e79599
davi.visintainer <davi.visintainer@placeti.com.br>
2022-11-11 17:10:04 -0300

    Implantando WebSecurity e configurando os primeiros acessos

---
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -0,0 +1,35 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+//    AO IMPLEMENTAR ESSA CLASSE, TUDO ESTÁ BLOQUEADO ATÉ QUE EU VENHA AQUI LIBERAR O ACESSO
+
+//    CONFIGURAR A PARTE DE AUTENTICAÇÃO
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    }
+
+//    CONFIGURAR A PARTE DE AUTORIZAÇÃO
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+//      ACIMA, ESTAMOS LIBERANDO ACESSO AS REQUISIÇÕES DO TIPO GET AOS ENDEREÇOS LISTADOS
+    }
+
+//    CONFIGURAR RECURSOS ESTÁTICOS - JS, CSS, IMG...
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+}
